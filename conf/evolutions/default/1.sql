789379a03d131c16923cd3fb1533d35ce915ce43
kunst1080 <kontrapunkt1080@gmail.com>
2016-10-08 23:27:38 +0900

    evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,22 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table t_user (
+  id                            bigint auto_increment not null,
+  name                          varchar(255),
+  school_year                   integer,
+  birth_day                     timestamp,
+  height                        integer,
+  food                          varchar(255),
+  created_at                    timestamp not null,
+  updated_at                    timestamp not null,
+  constraint pk_t_user primary key (id)
+);
+
+
+# --- !Downs
+
+drop table if exists t_user;
+
